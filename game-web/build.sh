4149934ca59e3cdce322a32fbe8a430ec3a54796
gernotfound <califano.ger@gmail.com>
2026-09-05 16:40:38 +0200

    fix: risolto problema target wasm32 su Vercel

--- game-web/build.sh
+++ game-web/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo "Installing Rust..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+echo "Installing wasm32 target for Rust..."
+rustup target add wasm32-unknown-unknown
 
 echo "Installing wasm-pack..."
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
