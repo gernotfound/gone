7a80db62408807e2d4a502fb0f668cbd938c1e93
Gerardo Califano <califano.ger@gmail.com>
2026-09-12 22:53:22 +0200

    Start smartphone profile before game runtime

--- game-web/src/main.ts
+++ game-web/src/main.ts
@@ -1,6 +1,8 @@
 import './style.css';
+import { startSmartphoneProfile } from './mobile/smartphoneProfile.ts';
 import { startPwaRuntime } from './pwa/pwaRuntime.ts';
 import { startClientRuntime } from './runtime/startClientRuntime.ts';
 
+startSmartphoneProfile();
 startPwaRuntime();
 startClientRuntime();
